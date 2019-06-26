d4ff69b2b308c81733618bdde2b90debec2627a8
Mark Oliver <mark.c.oliver@gmail.com>
2019-06-25 23:04:22 -0400

    postgresql

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # icn
 International Child Nutrition
 
-customer.txt has the SQL I used for MySql
-the 'length = 18' on line 30 of index.js is for LibreOffice, Excel likely will need a different length
+customer.txt has the SQL I used for Postgresql
+the 'length = 18' on line 58 of index.js is for LibreOffice, Excel likely will need a different length
--- index.js
+++ index.js
@@ -1,74 +1,102 @@
-const mysql = require('mysql');
+/**  const mysql = require('mysql'); */
+const { Client } = require('pg')
 const excel = require('exceljs');
-
+// postgres://juwwvohcorbolo:ce3d8c3e671044f8e15c880db71ce26146826e3f126da53a55788f7f0a7a54ae@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d4l0sqaog7kpe0
 // Create a connection to the database
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Mco75271|',
-    database: 'test'
-});
+
+const client = new Client({
+    user: 'juwwvohcorbolo',
+    host: 'ec2-23-23-173-30.compute-1.amazonaws.com',
+    database: 'd4l0sqaog7kpe0',
+    password: 'ce3d8c3e671044f8e15c880db71ce26146826e3f126da53a55788f7f0a7a54ae',
+    port: 5432,
+    ssl: true
+})
+
 
 function toXLSX(tableName) {
     // Open the MySQL connection
-    con.connect((err) => {
+    client.connect((err) => {
         if (err) throw err;
+    })
 
-        // -> Query data from MySQL
-        con.query(`SELECT * FROM ${tableName}`, function (err, data, fields) {
+    // -> Query data from Postgresql
+    client.query(`SELECT * FROM ${tableName}`, function (err, res) {
 
-            let workbook = new excel.Workbook(); //creating workbook
-            let worksheet = workbook.addWorksheet('Customers'); //creating worksheet
+        r = JSON.parse(JSON.stringify(res))
+        // console.log(r)
+        fields = r.fields
 
+        let workbook = new excel.Workbook(); //creating workbook
+        let worksheet = workbook.addWorksheet('Customers'); //creating worksheet
 
-            const columns = []
-            const date_columns = []
-            fields.forEach((item, i) => {
-                // console.log(item)
-                length = item.type
-                if (item.type === 10) {
-                    length = 18
-                    if (item.name.indexOf('date') >= 0) {
-                        date_columns.push(i)
-                    }
+        const max_lengths = {}
+        fields.forEach((item, __) => {max_lengths[item.name] = 0});
+        r.rows.forEach(row => {
+            // console.log('row', row)
+            for(const key in max_lengths) {
+                if (row[key].length > max_lengths[key]) {
+                    max_lengths[key] = row[key].length 
                 }
-                columns.push({ header: item.name, key: item.name, width: length })
             }
-            );
+        })
+        // console.log('max_lengths', max_lengths)
 
-            worksheet.columns = columns
+        const columns = []
+        const date_columns = []
+        fields.forEach((item, i) => {
+            // console.log(item)
+            length = max_lengths[item.name] + 1
 
-            // Add Array Rows
-            worksheet.addRows(JSON.parse(JSON.stringify(data)));
-            date_columns.forEach(i => {
-                column = worksheet.getColumn(i + 1)
-                column.eachCell((cell, ci) => {
-                    if (ci > 1) {
-                        // console.log(`cell.value: ${cell.value}`)
-                        cell.value = Date.parse(cell.value)
-                    }
-                })
-            })
+            if (length < 5) {
+                length = item.dataTypeSize === -1 ? item.dataTypeModifier : item.dataTypeSize;
+            }
+            // console.log('item.name', item.name,'length', length)
 
-            // worksheet.getCell('A1').value = new Date(2017, 2, 15);
-
-            // Write to File
-
-            workbook.xlsx.writeFile(`${tableName}.xlsx`)
-                .then(function () {
-                    console.log("file saved!");
-                });
-
-            // -> Close MySQL connection
-            con.end(function (err) {
-                if (err) {
-                    return console.log('error:' + err.message);
+            if (length ===  25) {
+                if (item.name.indexOf('date') >= 0) {
+                    length = 18
+                    date_columns.push(i)
                 }
-                console.log('Close the database connection.');
+            }
+            columns.push({ header: item.name, key: item.name, width: length })
+        });
+        // console.log('date_columns', date_columns)
+
+        worksheet.columns = columns
+        r.rows.sort(function(a, b){return a.id - b.id});
+
+        // Add Array Rows
+        worksheet.addRows(r.rows);
+        date_columns.forEach(i => {
+            column = worksheet.getColumn(i + 1)
+            column.eachCell((cell, ci) => {
+                if (ci > 1) {
+                    // console.log(`cell.value: ${cell.value}`)
+                    cell.value = Date.parse(cell.value)
+                }
+            })
+        })
+
+        // worksheet.getCell('A1').value = new Date(2017, 2, 15);
+
+        // Write to File
+
+        workbook.xlsx.writeFile(`${tableName}.xlsx`)
+            .then(function () {
+                console.log("file saved!");
             });
 
-            // -> Check 'customer.csv' file in root project folder
+        // -> Close MySQL connection
+        client.end(function (err) {
+            if (err) {
+                return console.log('error:' + err.message);
+            }
+            console.log('Close the database connection.');
         });
+
+        // -> Check 'customer.csv' file in root project folder
     });
+    //     });
 }
 toXLSX('customer')
